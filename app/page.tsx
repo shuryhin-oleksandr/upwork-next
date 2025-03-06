cc58bc69516c46fc3c3ee5bc24b4dfc5e4108010
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-03-06 02:04:31 +0200

    Make page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import { Button } from "antd";
+"use client";
+
+import { Layout, theme } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 export default function Home() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Content style={{ padding: "48px", maxWidth: "1200px", margin: "auto", width: "100%" }}>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+    </Layout>
   );
 }
