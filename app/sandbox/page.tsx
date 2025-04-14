f5aa47e1f2f76368c19d6d658aa06f9aa5ba1c29
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-04-14 22:10:12 +0300

    Make antd form for sandbox

--- app/sandbox/page.tsx
+++ app/sandbox/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { Button, Card, DatePicker } from "antd";
-import { Layout } from "antd";
+import { Button, Card, DatePicker, Form, FormProps, Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
 
 export default function Sandbox() {
@@ -9,13 +8,46 @@ export default function Sandbox() {
     <Layout style={{ minHeight: "100vh" }}>
       <Content style={{ padding: "30px", maxWidth: "1200px", margin: "auto", width: "100%" }}>
         <Card>
-          <DatePicker />
-          <br />
-          <br />
-          <Button>Save</Button>
+          <Tmp />
         </Card>
       </Content>
     </Layout>
   );
 }
 
+type FieldType = {
+  date?: string;
+};
+
+const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+function Tmp() { 
+  return (
+    <Form
+      name="basic"
+      style={{ maxWidth: 600 }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item<FieldType>
+        label="Date"
+        name="date"
+        rules={[{ required: true, message: "Please input your date!" }]}
+      >
+        <DatePicker format="D MMM YY" />
+      </Form.Item>
+
+      <Form.Item label={null}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
