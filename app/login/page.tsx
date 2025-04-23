e0d4a4340dd7938784303afecb81c8a07eaa7ad5
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-04-23 03:29:45 +0300

    Position login form appropriately

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,32 +9,37 @@ export default function Login() {
   };
 
   return (
-    <Card>
-      <Form
-        name="login"
-        initialValues={{ remember: true }}
-        style={{ maxWidth: 360 }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: "Please input your Username!" }]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-        </Form.Item>
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card title="Log in" style={{ width: "100%", maxWidth: 400 }}>
+        <Form name="login" onFinish={onFinish}>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+          </Form.Item>
 
-        <Form.Item>
-          <Button block type="primary" htmlType="submit">
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
-    </Card>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 }
