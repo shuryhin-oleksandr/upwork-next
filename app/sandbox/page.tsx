9aa7ed1433aa6ea9d3c29235d6fe2df2bd40382a
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-04-23 22:36:31 +0300

    Commit sandbox

--- app/sandbox/page.tsx
+++ app/sandbox/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { useMutation } from "@tanstack/react-query";
-import { Button, Card, DatePicker, Form, FormProps, Input, Layout } from "antd";
+import { Card, DatePicker, Form, Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
 
 export default function Sandbox() {
   return (
@@ -18,97 +15,40 @@ export default function Sandbox() {
   );
 }
 
-type FieldType = {
-  inputDate?: string;
-  stateInputDate?: string;
-};
-
-const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
-
 function Tmp() {
-  const [sentValues, setSentValues] = useState<FieldType | null>(null);
   const [form] = Form.useForm();
-  const [displayValue, setDisplayValue] = useState("");
-
-  const { mutate, error, data } = useMutation({
-    mutationFn: async (values: FieldType) => {
-      setSentValues(values);
-      const response = await fetch("http://localhost:3001/upwork/sandbox", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      });
-      return response.json();
-    },
-  });
-
-  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-    console.log("Submitting:", values);
-    mutate(values);
-  };
+  const formValues = Form.useWatch([], form);
 
   return (
-    <Form
-      form={form}
-      onValuesChange={() => console.log("Form:", form.getFieldsValue())}
-      name="basic"
-      style={{ maxWidth: 600 }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item<FieldType>
-        label="Input Date"
-        name="inputDate"
+    <Form form={form} name="basic" style={{ maxWidth: 600 }}>
+      <Form.Item
+        label="Date"
+        name="date"
         rules={[{ required: true, message: "Please input your date!" }]}
-        normalize={(value) => value + "AAA"}
       >
-        <Input
-          onChange={(e) => setDisplayValue(e.target.value)}
-          value={displayValue}
-        />
+        <DatePicker format={["D MMM YY"]} />
       </Form.Item>
-
-      <Form.Item<FieldType>
-        label="State input Date"
-        name="stateInputDate"
-        rules={[{ required: true, message: "Please input your date!" }]}
-        normalize={(value) => value + "AAA"}
-      >
-        <StateInput />
-      </Form.Item>
-
-      <Form.Item label={null}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-
-      {"Display value: " + displayValue}
-      {data && <p>Form data: {JSON.stringify(data)}</p>}
-      {error && <p>Error: {error.message}</p>}
-      {data && <br />}
-      {sentValues && <p>Sent Values: {JSON.stringify(sentValues)}</p>}
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {<p>Form data: {JSON.stringify(formValues)}</p>}
     </Form>
   );
 }
-
-function StateInput (props) {
-  const [internalValue, setInternalValue] = useState("");
-
-  console.log("Props:", props);
-
-  return (
-    <Input
-      {...props}
-      value={internalValue}
-      onChange={(e) => {
-        setInternalValue(e.target.value);
-        props.onChange?.(e);
-      }}
-    />
-  );
-};
