0924073b6c246f7985af4382095950530ca3c3b2
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-04-25 20:09:43 +0300

    Refactor api.ts, add accessToken interceptor

--- app/api.ts
+++ app/api.ts
@@ -1,33 +1,46 @@
 import { CreateRoomMetaDto, LoginDto, UpdateRoomMetaDto } from "@/app/interfaces";
 import axios from "axios";
 
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  console.log(config.headers);
+  return config;
+});
+
 export const getRooms = async () => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/upwork/rooms`;
-  const response = await axios.get(url);
+  const url = '/upwork/rooms';
+  const response = await api.get(url);
   return response.data;
 };
 
 export const updateRoomMeta = async (data: UpdateRoomMetaDto) => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/upwork/room-metas/${data._id}`;
-  const response = await axios.patch(url, data);
+  const url = `/upwork/room-metas/${data._id}`;
+  const response = await api.patch(url, data);
   return response.data;
 };
 
 export const createRoomMeta = async (data: CreateRoomMetaDto) => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/upwork/room-metas/`;
-  const response = await axios.post(url, data);
+  const url = '/upwork/room-metas/';
+  const response = await api.post(url, data);
   return response.data;
 };
 
 // TODO: Rationalise location
 export const login = async (data: LoginDto) => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`;
-  const response = await axios.post(url, data);
+  const url = '/auth/login';
+  const response = await api.post(url, data);
   return response.data;
 };
 
 export const refreshToken = async (refreshToken: string) => {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/refresh`;
-  const response = await axios.post(url, { refreshToken });
+  const url = '/auth/refresh';
+  const response = await api.post(url, { refreshToken });
   return response.data;
 };
