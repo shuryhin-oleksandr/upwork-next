d2c253e5f7cd635f1ff102997a3f2bea59cb3d97
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-05-06 21:59:29 +0300

    Fix logout button display bug

--- app/AppHeader.tsx
+++ app/AppHeader.tsx
@@ -20,6 +20,7 @@ export default function AppHeader() {
   const { data } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
+    enabled: isAuthenticated,
   });
 
   const logoutMutation = useMutation({
