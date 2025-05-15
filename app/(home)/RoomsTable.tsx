f41355f0a3a42b5cc8ca4af97825143017a15de3
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-05-15 13:14:24 +0300

    Upd naming

--- app/(home)/RoomsTable.tsx
+++ app/(home)/RoomsTable.tsx
@@ -271,12 +271,12 @@ const RoomsTable: React.FC = () => {
         if (!record.nextFollowUpDate)
           return <TypographyText style={{ color: token.colorPrimary }}>NEW</TypographyText>;
         else {
-          const followUpIndicator =
+          const followUpTypeSuffix =
             record.followUpDateType === "updated_after_client_message" ? " ^" : " *";
           return (
             <FollowUpDate
               date={dayjs(record.nextFollowUpDate)}
-              followUpIndicator={followUpIndicator}
+              followUpIndicator={followUpTypeSuffix}
             />
           );
         }
