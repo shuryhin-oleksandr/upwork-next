657e2c59343aac03b6842203be3012f9bd7cfa06
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-05-17 00:06:04 +0300

    Render bid stats table

--- app/bid-analysis/page.tsx
+++ app/bid-analysis/page.tsx
@@ -2,12 +2,37 @@
 
 import { getBidStatistics } from "@/app/bid-analysis/api";
 import { useQuery } from "@tanstack/react-query";
+import { Card, Table } from "antd";
+import { Typography } from "antd";
+
+const { Text } = Typography;
+
+const columns = [
+  {
+    title: "Job",
+    dataIndex: "title",
+  },
+  {
+    title: "Interviewing",
+    dataIndex: "interviewing",
+  },
+  {
+    title: "Hired",
+    dataIndex: "hired",
+  },
+];
 
 export default function BidAnalysis() {
-  const { data: jobs, isLoading } = useQuery({
+  const { data: jobs, isPending, isError, error } = useQuery({
     queryKey: ["bid-statistics"],
     queryFn: getBidStatistics,
   });
 
-  return <div>{JSON.stringify(jobs)}</div>;
+  if (isError) return <Text>Error: {error.message}</Text>;
+
+  return (
+    <Card>
+      <Table dataSource={jobs} columns={columns} size="small" rowKey="id" loading={isPending} />
+    </Card>
+  );
 }
