a327fdbb4d56a5a971057faf08aa8dc025f68957
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-05-17 01:54:51 +0300

    Add bid statystics

--- app/bid-analysis/page.tsx
+++ app/bid-analysis/page.tsx
@@ -2,8 +2,7 @@
 
 import { getBidStatistics } from "@/app/bid-analysis/api";
 import { useQuery } from "@tanstack/react-query";
-import { Card, Table } from "antd";
-import { Typography } from "antd";
+import { Card, Col, Row, Statistic, Table, Typography } from "antd";
 
 const { Text } = Typography;
 
@@ -26,6 +25,65 @@ const columns = [
   },
 ];
 
+function BidStatystics() {
+  const {
+    data: jobs,
+  } = useQuery({ queryKey: ["bid-statistics"], queryFn: getBidStatistics });
+
+  if (!jobs) return null;
+
+  const zeroRepliesJobsCount = jobs.filter((job) => job.interviewing === 0).length;
+  const oneTwoRepliesJobsCount = jobs.filter(
+    (job) => job.interviewing > 0 && job.interviewing <= 2
+  ).length;
+  const threePlusRepliesJobsCount = jobs.filter((job) => job.interviewing >= 3).length;
+  const hiredJobsCount = jobs.filter((job) => job.hired > 0).length;
+  return (
+    <Row gutter={16}>
+      <Col span={3}>
+        <Card>
+          <Statistic
+            title="0 replies"
+            value={(zeroRepliesJobsCount / jobs.length) * 100}
+            suffix="%"
+            precision={0}
+          />
+        </Card>
+      </Col>
+      <Col span={3}>
+        <Card>
+          <Statistic
+            title="1-2 replies"
+            value={(oneTwoRepliesJobsCount / jobs.length) * 100}
+            suffix="%"
+            precision={0}
+          />
+        </Card>
+      </Col>
+      <Col span={3}>
+        <Card>
+          <Statistic
+            title="3+ replies"
+            value={(threePlusRepliesJobsCount / jobs.length) * 100}
+            suffix="%"
+            precision={0}
+          />
+        </Card>
+      </Col>
+      <Col span={3}>
+        <Card>
+          <Statistic
+            title="Hired"
+            value={(hiredJobsCount / jobs.length) * 100}
+            suffix="%"
+            precision={0}
+          />
+        </Card>
+      </Col>
+    </Row>
+  );
+}
+
 export default function BidAnalysis() {
   const {
     data: jobs,
@@ -37,19 +95,22 @@ export default function BidAnalysis() {
   if (isError) return <Text>Error: {error.message}</Text>;
 
   return (
-    <Card>
-      <Table
-        dataSource={jobs}
-        columns={columns}
-        size="small"
-        rowKey="id"
-        loading={isPending}
-        pagination={{
-          showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
-          showSizeChanger: true,
-          defaultPageSize: 50,
-        }}
-      />
-    </Card>
+    <>
+      <BidStatystics />
+      <Card style={jobs && { marginTop: "2rem" }}>
+        <Table
+          dataSource={jobs}
+          columns={columns}
+          size="small"
+          rowKey="id"
+          loading={isPending}
+          pagination={{
+            showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items`,
+            showSizeChanger: true,
+            defaultPageSize: 50,
+          }}
+        />
+      </Card>
+    </>
   );
 }
