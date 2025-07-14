23943cef13f5722238095e7903fec32ffa8c197d
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-07-15 01:33:37 +0300

    Fix locale

--- app/lib/AntdProvider.tsx
+++ app/lib/AntdProvider.tsx
@@ -3,12 +3,17 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "@ant-design/v5-patch-for-react-19";
 import { App, ConfigProvider, theme } from "antd";
+import enGB from "antd/locale/en_GB";
+import dayjs from "dayjs";
+import "dayjs/locale/en-gb";
+dayjs.locale("en-gb");
 
 export default function AntdProvider({ children }: { children: React.ReactNode }) {
   const isDark = false;
   return (
     <AntdRegistry>
       <ConfigProvider
+        locale={enGB}
         theme={{
           algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
         }}
