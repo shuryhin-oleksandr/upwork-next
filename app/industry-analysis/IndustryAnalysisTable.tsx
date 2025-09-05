c7239fe5bbfb2477b1a2934685e17332ce65e487
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-09-05 10:44:35 +0300

    Make job title an upwork link

--- app/industry-analysis/IndustryAnalysisTable.tsx
+++ app/industry-analysis/IndustryAnalysisTable.tsx
@@ -4,9 +4,11 @@ import { useQuery } from "@tanstack/react-query";
 import type { TableProps } from "antd";
 import { Flex, Table, Tag } from "antd";
 import { getJobsWithMeta } from "./api";
+import JobUrl from "@/app/components/JobUrl";
 
 interface DataType {
   id: string;
+  upworkId: string;
   title: string;
   description: string;
   industry: number;
@@ -16,6 +18,7 @@ const columns: TableProps<DataType>["columns"] = [
   {
     title: "Title",
     dataIndex: "title",
+    render: (text, record) => <JobUrl title={text} upworkId={record.upworkId} />,
   },
   {
     title: "Industry",
@@ -23,9 +26,7 @@ const columns: TableProps<DataType>["columns"] = [
     render: (industries) => (
       <Flex wrap="wrap" gap="small">
         {industries.map((industry: string, index: number) => (
-          <Tag key={index}>
-            {industry}
-          </Tag>
+          <Tag key={index}>{industry}</Tag>
         ))}
       </Flex>
     ),
@@ -42,7 +43,9 @@ export default function IndustryAnalysisTable() {
     <Table<DataType>
       columns={columns}
       expandable={{
-        expandedRowRender: (record) => <p style={{ margin: 0 }}>{record.description}</p>,
+        expandedRowRender: (record) => (
+          <p style={{ margin: 0, whiteSpace: "pre-wrap" }}>{record.description}</p>
+        ),
         rowExpandable: (record) => !!record.description,
       }}
       dataSource={jobsWithMeta}
