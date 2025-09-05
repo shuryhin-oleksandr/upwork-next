b7fb58e97c362c711214a766a4d652dc9af26b67
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-09-05 11:43:09 +0300

    Render techStack and industryKeywords

--- app/industry-analysis/IndustryAnalysisTable.tsx
+++ app/industry-analysis/IndustryAnalysisTable.tsx
@@ -2,9 +2,11 @@
 
 import { useQuery } from "@tanstack/react-query";
 import type { TableProps } from "antd";
-import { Flex, Table, Tag } from "antd";
+import { Flex, Table, Tag, Typography } from "antd";
 import { getJobsWithMeta } from "./api";
 import JobUrl from "@/app/components/JobUrl";
+
+const { Text } = Typography;
 
 interface DataType {
   id: string;
@@ -12,6 +14,11 @@ interface DataType {
   title: string;
   description: string;
   industry: number;
+  meta: {
+    industry: string[];
+    industryKeywords: string[];
+    techStack: string[];
+  };
 }
 
 const columns: TableProps<DataType>["columns"] = [
@@ -44,7 +51,27 @@ export default function IndustryAnalysisTable() {
       columns={columns}
       expandable={{
         expandedRowRender: (record) => (
-          <p style={{ margin: 0, whiteSpace: "pre-wrap" }}>{record.description}</p>
+          <Flex gap={"large"}>
+            <p style={{ margin: 0, whiteSpace: "pre-wrap" }}>{record.description}</p>
+            <Flex vertical gap={"large"}>
+              <Flex vertical>
+                <Text strong>Industry Keywords</Text>
+                {record.meta.industryKeywords.map((keyword, index) => (
+                  <Text key={index} style={{ whiteSpace: "nowrap" }}>
+                    {keyword}
+                  </Text>
+                ))}
+              </Flex>
+              <Flex vertical>
+                <Text strong>Tech Stack</Text>
+                {record.meta.techStack.map((keyword, index) => (
+                  <Text key={index} style={{ whiteSpace: "nowrap" }}>
+                    {keyword}
+                  </Text>
+                ))}
+              </Flex>
+            </Flex>
+          </Flex>
         ),
         rowExpandable: (record) => !!record.description,
       }}
