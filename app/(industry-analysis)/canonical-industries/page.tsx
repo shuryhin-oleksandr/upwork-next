85fd22dd475a11206b5ffe26bad70840063c056d
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-09-10 12:02:17 +0300

    Add jobsCount

--- app/(industry-analysis)/canonical-industries/page.tsx
+++ app/(industry-analysis)/canonical-industries/page.tsx
@@ -8,6 +8,7 @@ interface CanonicalIndustry {
   id: string;
   name: string;
   aliases: string[];
+  jobCount: number;
 }
 
 export default function CanonicalIndustries() {
@@ -18,11 +19,16 @@ export default function CanonicalIndustries() {
 
   return (
     <Card style={{ marginTop: 16 }}>
-      {canonicalIndustries.map((canonicalIndustry) => (
-        <div key={canonicalIndustry.id} style={{ marginBottom: 8 }}>
-          <strong>{canonicalIndustry.name}:</strong> {canonicalIndustry.aliases.join(", ")}
-        </div>
-      ))}
+      {canonicalIndustries
+        .sort((a, b) => b.jobCount - a.jobCount)
+        .map((canonicalIndustry) => (
+          <div key={canonicalIndustry.id} style={{ marginBottom: 8 }}>
+            <strong>
+              {canonicalIndustry.jobCount} - {canonicalIndustry.name}:
+            </strong>{" "}
+            {canonicalIndustry.aliases.join(", ")}
+          </div>
+        ))}
     </Card>
   );
 }
