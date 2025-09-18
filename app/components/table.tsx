3fdd6440c13ef714bbf5c86c9884ae0c225543ca
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2025-09-18 21:40:32 +0300

    Add EditableRow to components

---
+++ app/components/table.tsx
@@ -0,0 +1,21 @@
+import { Form, GetRef } from "antd";
+import React from "react";
+
+type FormInstance<T> = GetRef<typeof Form<T>>;
+
+const EditableContext = React.createContext<FormInstance<any> | null>(null);
+
+interface EditableRowProps {
+  index: number;
+}
+
+const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
+};
